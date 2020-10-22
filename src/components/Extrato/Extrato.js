ce0cb97ce93fb4958caae1c2269d757bcf5af692
Marcus Pontes <marcusporama@gmail.com>
2020-10-22 00:01:10 -0300

    Finish

--- src/components/Extrato/Extrato.js
+++ src/components/Extrato/Extrato.js
@@ -6,11 +6,20 @@ import "./Extrato.css"
 import PostDataEmp from "../../mocks/transacoes.json"
 
 function Extrato() {
+  const empresaFiltered = PostDataEmp.filter((emp) => emp.empresaId === 1)
+  console.log(empresaFiltered)
+
+  function impValores(e, i, a) {
+    console.log("passou")
+  }
+
   return (
     <div className="App-headerExtrato">
       <Card border="primary" style={{ width: "18rem" }} className="cardExtrato">
         <Card.Header>Detalhamento</Card.Header>
-        <Card.Body></Card.Body>
+        <Card.Body>
+          <Card.Text>DETALHES</Card.Text>
+        </Card.Body>
       </Card>
     </div>
   )
